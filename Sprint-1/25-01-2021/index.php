5c870faf19d3f3718ef5968738b422c906c797c9
syofyanzuhad <sofyanzuhad2@gmail.com>
2021-01-25 10:20:23 +0700

    hari-1

---
+++ Sprint-1/25-01-2021/index.php
@@ -0,0 +1,27 @@
+<?php
+
+// echo "hello world ! \n";
+
+// $hello = "hello world ! \n";
+// echo $hello;
+
+// echo 10%3;
+
+// $a = 3;
+// $a+=3;
+// echo $a."\n";
+
+// var_dump(2<>3);
+
+// var_dump("a"=="A");
+
+// var_dump((2==3) && (2==2));
+
+$email = "admin@gmail.com";
+$password = "123";
+
+if ($email == "admin@gmail.com" && $password == "123") {
+   echo "Login berhasil ! \n";
+} else {
+   echo "Login gagal ! \n";
+}
