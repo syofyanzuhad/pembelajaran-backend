c547528008ea6248a9a0896c17156963c73b0953
syofyanzuhad <sofyanzuhad2@gmail.com>
2021-01-27 10:54:35 +0700

    hari-3

---
+++ Sprint-1/27-01-2021/index.php
@@ -0,0 +1,36 @@
+<?php
+
+// $array = array("satu", "dua", 5, true);
+
+// $array = ["Array", "numeric", "asd", "vbxcx", "erqrwqe"];
+
+// $array[2] = "index";
+
+$array = [
+   'satu' => "index 1",
+   'dua' => "index 2",
+];
+
+// $array = [
+//    'array 1' => [
+//       'dimensi 2' => [
+//          'dimensi 3' => 'array 3'
+//       ]
+//    ]
+// ];
+
+// for ($i=0; $i < count($array); $i++) { 
+//    echo $array[$i]."\n";
+// }
+
+// echo $array[0];
+
+// print_r($array);
+
+// foreach($array as $key => $value) {
+//    echo $key ." => ". $value."\n";
+// }
+
+$a = 1;
+
+($a>5) ? "variable a > 5" : "variable a < 5";
